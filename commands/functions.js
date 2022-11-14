e349f83572ac32a7589fe42c8aaef71af9e30067
Gwerh <lalo@perronegro.cl>
2022-11-14 15:21:40 -0300

    actualización

---
+++ commands/functions.js
@@ -0,0 +1,18 @@
+export async function embedReq(title){
+    const embedDatos = new Discord.MessageEmbed() 
+    .setTitle(title)
+    .setAuthor(message.author.username, message.author.displayAvatarURL())
+    .setColor(0xFFE100)
+    // .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
+    // .setFooter("Pie de página, puede contener 1024 caracteres", client.user.avatarURL())
+    // .setImage(message.author.displayAvatarURL())
+    // .setThumbnail(message.author.displayAvatarURL())
+    // .setTimestamp()
+    .setURL("https://docs.google.com/spreadsheets/d/e/2PACX-1vQXjr-Ru-P6-__EqRLFSsRs0f3NBS7rgxeQSIFGZU7tMKHZoKkJbNK1Ju1bAVPqRYbqgTKdkILz9TWB/pubhtml")
+    // .addField("Este es un título de campo", "Este es un valor de campo puede contener 1024 caracteres.")
+    // .addField("Campo en línea", "Debajo del campo en línea",  true)
+    // .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
+    const msg = message
+    message.delete(this.message)
+    await msg.reply({ embed: embedDatos })
+}
--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 const Discord = require("discord.js");
+const { embedReq } = require("./commands/functions");
 const config = require("./config.json");
 const client = new Discord.Client();
 const prefix = "q-";
 
 client.on('ready', ()=>{
-    var generalChannel = client.channels.cache.get("1038269083336052838")
-    generalChannel.send("Bienvenidos a World of Gwerh")
+    const generalChannel = client.channels.cache.get("1038269083336052838")
+    generalChannel.send(`Ha llegado el escudero`)
 })
 
 client.on("message", async function(message) {
     if (message.author.bot) return;
-    if (message.content === "escudero"){
+    if (message.content === `${prefix} escudero`){
         await message.author.send(`Esta es la lista de comandos`)
+    }
+    if (message.content === `${prefix} quests`){
+        await message.author.send(`Quests activas`)
     }
     // if (!message.content.startsWith(prefix)) return;
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    if (command === "ainusys" || message.content === "ainusys") {
-        const embedDatos = new Discord.MessageEmbed() 
-        .setTitle("Este es su título, puede contener 256 caracteres")
-        .setAuthor(message.author.username, message.author.displayAvatarURL())
-        .setColor(0x00AE86)
-        .setDescription("Este es el cuerpo principal del texto, puede contener 2048 caracteres.")
-        .setFooter("Pie de página, puede contener 1024 caracteres", client.user.avatarURL())
-        .setImage(message.author.displayAvatarURL())
-        .setThumbnail(message.author.displayAvatarURL())
-        .setTimestamp()
-        .setURL("https://docs.google.com/spreadsheets/d/e/2PACX-1vQXjr-Ru-P6-__EqRLFSsRs0f3NBS7rgxeQSIFGZU7tMKHZoKkJbNK1Ju1bAVPqRYbqgTKdkILz9TWB/pubhtml")
-        .addField("Este es un título de campo", "Este es un valor de campo puede contener 1024 caracteres.")
-        .addField("Campo en línea", "Debajo del campo en línea",  true)
-        .addField("Campo en línea 3", "Puede tener un máximo de 25 campos.", true);
-        await message.reply({ embed: embedDatos })
+    if (command === "[sistema]" || message.content === "[sistema]") {
+        embedReq("[Sistema Ainulindalë]")
     }
   });
 
-client.login(config.BOT_TOKEN);
+client.login(config.BOT_TOKEN, {autorun: true});
